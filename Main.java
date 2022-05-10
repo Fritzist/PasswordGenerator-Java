115ab4405864659d6e5110514c7588a4fcc64e49
Fritzist <danielhartl856@gmail.com>
2022-05-10 16:59:25 +0200

    added numbers / fixed bugs and added symbols

--- Main.java
+++ Main.java
@@ -8,15 +8,17 @@ public class Main {
 
         String lower_cases = "qwertzuiopasdfghjklyxcvbnm";
         String upper_cases = "QWERTZUIOPASDFGHJKLYXCVBNM";
+        String symbols = "!§$%&/()=?,.;:-_#+*'~{[]}\\";
+
 
         String password = "";
 
         for(int i = 0; i < digit; i++) {
-            int rand = (int)(2 * Math.random());
+            int rand = (int)(4 * Math.random());
 
             switch (rand) {
                 case 0:
-                    password += String.valueOf((int) (0 * Math.random()));
+                    password += String.valueOf((int) (10 * Math.random()));
                     break;
                 case 1:
                     rand = (int) (lower_cases.length() * Math.random());
@@ -25,6 +27,10 @@ public class Main {
                 case 2:
                     rand = (int) (upper_cases.length() * Math.random());
                     password += String.valueOf(upper_cases.charAt(rand));
+                    break;
+                case 3:
+                    rand = (int) (symbols.length() * Math.random());
+                    password += String.valueOf(symbols.charAt(rand));
                     break;
             }
         }
